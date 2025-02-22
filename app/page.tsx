8c61534761fcb7bbf7ae6de060dda85134906e66
benjamin-mahmood <benmahmood@icloud.com>
2025-02-22 12:03:47 +0100

    reset nextjs to use heroui and tailwind 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1 className='text-3xl text-center mt-48 font-bold underline'>
-          Hallo Welt! Das ist eine automatisch deployed NextJS 15 App mit
-          Coolify! 🚀
-        </h1>
-      </div>
+      <div>Hello world!</div>
     </main>
-  )
+  );
 }
