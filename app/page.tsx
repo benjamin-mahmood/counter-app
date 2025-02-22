feead01323340e90290cd1fdaffe4bf5d363395b
benjamin-mahmood <benmahmood@icloud.com>
2025-02-22 12:27:16 +0100

    installed heroui with dark mode and added a button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@heroui/button'
+import { ThemeToggle } from './components/ThemeToggle'
+
 export default function Home() {
   return (
     <main>
+      <ThemeToggle />
       <div>Hello world!</div>
+      <Button>Klick Mich</Button>
     </main>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+const { heroui } = require('@heroui/react')
 
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
-  plugins: [],
-} satisfies Config;
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [heroui()],
+} satisfies Config
