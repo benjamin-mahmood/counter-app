c7bf6f693283698c53bc11faa89afe6bccaead8b
benjamin-mahmood <benmahmood@icloud.com>
2025-02-22 13:06:59 +0100

    added heroui with dark mode and a button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@heroui/button'
-import { ThemeToggle } from './components/ThemeToggle'
+import { ThemeToggle } from '@/components/ThemeToggle'
 
 export default function Home() {
   return (
     <main>
       <ThemeToggle />
       <div>Hello world!</div>
-      <Button>Klick Mich</Button>
+      <Button color='primary'>Klick Mich</Button>
     </main>
   )
 }
